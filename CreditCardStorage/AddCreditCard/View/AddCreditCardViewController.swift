1e5cb1cb9c2b5f0c6270ecf7b7b9c63748c97d3f
Дмитрий <dapavlov16@yandex.ru>
2020-08-26 12:23:04 +0300

    titled text field

--- CreditCardStorage/AddCreditCard/View/AddCreditCardViewController.swift
+++ CreditCardStorage/AddCreditCard/View/AddCreditCardViewController.swift
@@ -17,13 +17,41 @@ protocol AddCreditCardViewInput: AnyObject {
 }
 
 final class AddCreditCardViewController: UIViewController {
-
+    
     var presenter: AddCreditCardViewOutput?
+    private let textField = TitledTextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
+        
+        view.backgroundColor = .white
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        textField.viewModel = TitledTextFieldViewModel(titleText: "blabla",
+                                                       textFieldPlaceholderText: "ablabla",
+                                                       keyboardType: UIKeyboardType.default,
+                                                       isSecure: true)
+    }
+    
+    @objc func rotated() {
+//        view.layoutIfNeeded()
+//        view.setNeedsLayout()
+        textField.layoutIfNeeded()
+        textField.setNeedsLayout()
     }
 }
 
---
+++ CreditCardStorage/TitledTextField/TitledTextFieldViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TitledTextFieldViewModel.swift
+//  CreditCardStorage
+//
+//  Created by Дмитрий Павлов on 26.08.2020.
+//  Copyright © 2020 Gregory Pinetree. All rights reserved.
+//
+
+import UIKit
+
+struct TitledTextFieldViewModel {
+    let titleText: String
+    let textFieldPlaceholderText: String
+    let keyboardType: UIKeyboardType
+    let isSecure: Bool
+}
